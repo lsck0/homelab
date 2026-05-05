a71dfc18ffca80210b2d458bc2249b4372e6038f
Luca Sandrock <luca.sandrock@proton.me>
2026-05-05 21:39:56 +0200

    chore(deploy): generation 82

--- src/modules/traefik.nix
+++ src/modules/traefik.nix
@@ -124,6 +124,9 @@ in {
           {
             certFile = "/var/lib/traefik/certs/server-cert.pem";
             keyFile = "/var/lib/traefik/certs/server-key.pem";
+            domains = [
+              { main = "lsck0.dev"; sans = [ "*.lsck0.dev" ]; }
+            ];
           }
         ];
       };
